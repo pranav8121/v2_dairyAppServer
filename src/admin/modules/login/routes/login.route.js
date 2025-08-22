791b93418202a6d1480a560b600e6525f0252611
Pranav Patil <pranav.patil@vernost.com>
2025-08-23 00:22:33 +0530

    file name corrected

--- src/admin/modules/login/routes/login.route.js
+++ src/admin/modules/login/routes/login.route.js
@@ -2,7 +2,7 @@
 let router = require('express').Router();
 const jwt = require("jsonwebtoken");
 
-let loginService = new (require("../service/member.service"))();
+let loginService = new (require("../service/login.service"))();
 const responseHandler= require('../../../../../response-handler/response-handler');
 
 router.post('/authenticate', responseHandler(loginService.authenticate));
